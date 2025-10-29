1a935ca68c60f5302aa2d0b78e140670c5a3857b
Francesco Refolli <f.refolli@campus.unimib.it>
2025-10-29 23:16:07 +0100

    ok

--- listings/approach/cheating/operator-overloading.cc
+++ listings/approach/cheating/operator-overloading.cc
@@ -1,5 +1,4 @@
 void main() {
-  Vector3 A = Vector3 {.x = 0, .y = 0, .z = 1};
-  Vector3 B = Vector3 {.x = 1, .y = 0, .z = 0};
+  Vector3 A = Vector3 {0,0,1}, B = Vector3 {1,0,0};
   Vector3 C = A + B; // A.operator+(B)
 }
